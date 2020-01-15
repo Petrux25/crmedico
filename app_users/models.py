983cb11af87f158355f2f3dc7843f67c47bbbf49
Diego Velásquez <diesveca@hotmail.com>
2020-01-15 17:50:19 -0600

    Added users model

--- app_users/models.py
+++ app_users/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class MedicalUser(User):
+    SPECIALTIES = (
+        (1, 'Small'),
+        (2, 'Medium'),
+        (3, 'Large'),
+    )
+    med_id = models.CharField(max_length = 8)
+    phone = models.CharField(max_length = 8)
+    specialty = models.IntegerField(choices = SPECIALTIES)
+    specialty_desc = models.TextField()
+
+class CommonUser(User):
+    phone = models.CharField(max_length = 8)
