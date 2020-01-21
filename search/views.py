4d3b5cec4ac8a83634f4898536a6deabe8d43128
Diego Velásquez <diesveca@hotmail.com>
2020-01-21 15:16:25 -0600

    Finished search all without parameters

--- search/views.py
+++ search/views.py
@@ -5,9 +5,9 @@ from app_users.models import MedService
 def search(request, page_number=1):
     first = (page_number - 1) * 10
     last = first + 10
-    meds = map(mapUsers, MedService.objects.all()[first:last])
+    meds = list(map(mapUsers, MedService.objects.all()[first:last]))
     size = len(meds)
-    return render(request, "search.html", {'meds': meds, 'size': size, 'page_number': page_number})
+    return render(request, "search.html", {'meds': meds, 'size': size, 'page_number': page_number, 'next':page_number+1, 'prev':page_number-1})
 
 
 def mapUsers(medServ):
