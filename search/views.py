2a5427852121da7eb1bac765a05c59ffcf6e67c2
Diego Velásquez <diesveca@hotmail.com>
2020-01-21 19:13:25 -0600

    Search completed

--- search/views.py
+++ search/views.py
@@ -2,12 +2,24 @@ from django.shortcuts import render
 from app_users.models import MedService
 
 def search(request):
-    return render(request, "search.html")
+    specialties = MedService.SPECIALTIES
+    provinces = MedService.PROVINCES
+    return render(request, "search.html", {'specialties': specialties, 'provinces': provinces})
 
 def result(request, page_number=1):
+    all_meds = MedService.objects.all()
+    description = request.GET['description']
+    province = request.GET['province']
+    specialty = request.GET['specialty']
+    variables = {'description': description, 'province': province, 'specialty':specialty}
+    arguments = {}
+    for k, v in variables.items():
+        if v:
+            arguments[k] = v
+    all_meds = all_meds.filter(**arguments)
     first = (page_number - 1) * 10
     last = first + 10
-    meds = list(map(mapUsers, MedService.objects.all()[first:last]))
+    meds = list(map(mapUsers, all_meds[first:last]))
     size = len(meds)
     return render(request, "result.html", {'meds': meds, 'size': size, 'page_number': page_number, 'next':page_number+1, 'prev':page_number-1})
 
