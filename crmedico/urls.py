cba2d3eea86324c9ec9b27c85c02dc52c44eb67e
Diego Velásquez <diesveca@hotmail.com>
2020-01-22 00:44:38 -0600

    Updated nav and urls

--- crmedico/urls.py
+++ crmedico/urls.py
@@ -1,10 +1,12 @@
 from django.contrib import admin
 from django.urls import path, include
-from app_users import views
+from app_users import views as app_users_views
 from search import views
 from reservations import views
 
+
 urlpatterns = [
+    path('', app_users_views.login_page),
     path('admin/', admin.site.urls),
     path('users/', include('app_users.urls')),
     path('search/', include('search.urls'), name="search"),
